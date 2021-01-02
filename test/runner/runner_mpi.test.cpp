b429a85be28b9cf21d27265c4bad85df2250557c
APN-Pucky <APN-Pucky@users.noreply.github.com>
2021-01-02 18:51:15 +0100

    Add processor print debug info

--- test/runner/runner_mpi.test.cpp
+++ test/runner/runner_mpi.test.cpp
@@ -36,9 +36,13 @@ TEST_CASE( "mpi array add2", "[mpi]" ) {
     // Get the rank of the process
     int world_rank;
     MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
+    // Get the name of the processor
+    char processor_name[MPI_MAX_PROCESSOR_NAME];
+    int name_len;
+    MPI_Get_processor_name(processor_name, &name_len);
     printf("Hello world from processor %s, rank %d out of %d processors\n",
            processor_name, world_rank, world_size);
-    if(mpi_rank==0) {
+    if(world_rank==0) {
         printf("\n");
         for(int i = 0; i < N;++i) {
             printf("%d",c[i]);
