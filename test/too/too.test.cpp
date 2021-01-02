f31e158eec12cfd72e2382056bbe86e429417bd9
APN-Pucky <APN-Pucky@users.noreply.github.com>
2021-01-02 20:43:01 +0100

    Add too mpi test

--- test/too/too.test.cpp
+++ test/too/too.test.cpp
@@ -5,7 +5,7 @@
 #include "runner.h"
 #include "tuo.h"
 #include "../../src/tuo/h/tyrant_optimize.h"
-TEST_CASE( "single", "[single]" ) {
+TEST_CASE( "too single", "[single]" ) {
     //int all_cards_size;
     //CLCard* all_cards = load_all_cards(&all_cards_size);
     int all_cards_size = 3;
@@ -62,6 +62,7 @@ TEST_CASE( "single", "[single]" ) {
     //r.run_openmp();
     //r.run_hybrid();
     //r.run_cpu();
+    //r.run_mpi();
 
     for (int i = 0; i  < N; ++i) printf("%d",results[i]);
 
@@ -73,7 +74,95 @@ TEST_CASE( "single", "[single]" ) {
 
     REQUIRE (0 >= 0);
 }
-TEST_CASE( "test mpclhy", "[mpclhy]" ) {
+TEST_CASE( "too mpi", "[mpclhy]" ) {
+    //int all_cards_size;
+    //CLCard* all_cards = load_all_cards(&all_cards_size);
+    int all_cards_size = 3;
+    int all_cards_size_ = 3;
+    CLCard all_cards[all_cards_size];
+    load_all_cards(&all_cards_size_);
+    printf("Loaded %d cards", all_cards_size);
+    //const unsigned int size_card=10+4*3*size_skillspec+Skill::num_skills*2+2;
+    const unsigned int size_all_cards = all_cards_size*size_card;
+    //const unsigned int size_deck=10;
+    const unsigned int sims = 1024000;
+    const unsigned int N = sims;
+    const unsigned long seed = 5;
+
+    int mydeck[size_deck];
+    int enemydeck[size_deck];
+    int* allcards = (int*)all_cards;
+    copy_card(&all_cards[0] , tuo::all_cards.cards_by_id[1]);
+    copy_card(&all_cards[1] , tuo::all_cards.cards_by_id[34]);
+    copy_card(&all_cards[2] , tuo::all_cards.cards_by_id[1000]);
+    //for( int i =0; i < size_all_cards;++i) {
+    //    allcards[i] = 0;
+    //}
+    //allcards[0] = 2;
+    //allcards[1] = 2;
+    //allcards[2] = 2;
+    //allcards[4] = 2;
+    //allcards[5] = 2;
+
+    //allcards[size_card +0] = 1;
+    //allcards[size_card +1] = 1;
+    //allcards[size_card +2] = 1;
+    //allcards[size_card +4] = 1;
+    //allcards[size_card +5] = 1;
+
+    int results[N];
+    mydeck[0] = 1000;
+    enemydeck[0] = 1000;
+    for(int i =2; i  < size_deck ;++i) {
+	    mydeck[i] = 34;
+	    enemydeck[i] = 1;
+    }
+    for(int i =0; i  < N; ++i) {
+	    results[i] = -2;
+    }
+
+    auto r = Runner<int,int*,int,int*,int,int*,int,int*,int,int>("simulate",simulate,N,size_all_cards,allcards,size_deck,mydeck,size_deck,enemydeck,N,results,N,seed);
+    r.set_mem<1>(CL_MEM_READ_ONLY,size_all_cards,false);
+    r.set_mem<3>(CL_MEM_READ_ONLY,size_deck,false);
+    r.set_mem<5>(CL_MEM_READ_ONLY,size_deck,false);
+    r.set_mem<7>(CL_MEM_WRITE_ONLY,N,true);
+
+    //r.run_noparallel();
+    //r.run_openmp();
+    //r.run_hybrid();
+    auto start = std::chrono::high_resolution_clock::now();
+    r.run_mpi();
+
+    auto finish = std::chrono::high_resolution_clock::now();
+    std::chrono::duration<double> elapsed = finish - start;
+    std::cout << "\nMPI == > Elapsed time: " << elapsed.count() << " s\n";
+    // Get the number of processes
+    int world_size;
+    MPI_Comm_size(MPI_COMM_WORLD, &world_size);
+
+    // Get the rank of the process
+    int world_rank;
+    MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
+    // Get the name of the processor
+    char processor_name[MPI_MAX_PROCESSOR_NAME];
+    int name_len;
+    MPI_Get_processor_name(processor_name, &name_len);
+    printf("Hello world from processor %s, rank %d out of %d processors",
+           processor_name, world_rank, world_size);
+    for (int i = 0; i  < N/1000 ; ++i) printf("%d",results[i]);
+    printf("\n");
+
+    //r.run_cpu();
+    //printf("\nsingle run %d\n",results[0]);
+
+    if(world_rank==0) {
+        for(int i =0 ; i  < N;++i) {
+            REQUIRE (results[i] == -1);
+        }
+    }
+
+}
+TEST_CASE( "too mpclhy", "[mpclhy]" ) {
     //int all_cards_size;
     //CLCard* all_cards = load_all_cards(&all_cards_size);
     int all_cards_size = 3;
